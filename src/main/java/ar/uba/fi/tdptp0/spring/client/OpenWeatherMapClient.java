e2fa52e088f21b8e015a115e6b5757a1644ad19f
Fernando Cancellara <fdcancellara@gmail.com>
2018-03-18 19:50:09 -0300

    Api key in application.yml

--- src/main/java/ar/uba/fi/tdptp0/spring/client/OpenWeatherMapClient.java
+++ src/main/java/ar/uba/fi/tdptp0/spring/client/OpenWeatherMapClient.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "weather-api", url = "https://api.openweathermap.org/data/2.5")
 public interface OpenWeatherMapClient {
 
-    @GetMapping(value = "/forecast?id={cityId}&appid=b14c689324c9d7fa0e17735d6fe38087", produces = {"application/json"})
+    @GetMapping(value = "/forecast?id={cityId}&appid=${app.api-key}", produces = {"application/json"})
     String getWeather(@PathVariable("cityId") long cityId);
 }
