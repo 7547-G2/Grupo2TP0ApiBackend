016b042f64eff03942dc5ef8e8928ed7e9c893f9
Fernando Cancellara <fdcancellara@gmail.com>
2018-03-19 00:04:42 -0300

    Refactor of weather parsing

--- src/main/java/ar/uba/fi/tdptp0/service/weather/OpenWeatherMapWeatherService.java
+++ src/main/java/ar/uba/fi/tdptp0/service/weather/OpenWeatherMapWeatherService.java
@@ -1,19 +1,14 @@
 package ar.uba.fi.tdptp0.service.weather;
 
 import ar.uba.fi.tdptp0.spring.client.OpenWeatherMapClient;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.json.simple.JSONObject;
-import java.util.Date;
-import java.util.List;
 import java.util.ArrayList;
-import com.google.gson.JsonArray;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.ParseException;
-import org.json.simple.parser.JSONParser;
+import java.util.Iterator;
+import java.util.List;
 
 public class OpenWeatherMapWeatherService implements WeatherService {
 
@@ -27,89 +22,126 @@ public class OpenWeatherMapWeatherService implements WeatherService {
     @Override
     public String forecastData(Long id) {
         String jsonString = openWeatherMapClient.getWeather(id);
+        String parsedJson = parseResponse(jsonString);
+
+        return parsedJson;
+    }
+
+    private String parseResponse(String jsonString) {
         JSONParser parser = new JSONParser();
-        JSONArray listOf5Days = new JSONArray();
-        JSONObject responseJson = new JSONObject();
-        JSONObject json = new JSONObject();
+        JSONObject weatherJson;
         try {
-            json = (JSONObject) parser.parse(jsonString);
+            weatherJson = (JSONObject) parser.parse(jsonString);
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            return "{}";
         }
-        JSONArray listOfDays = (JSONArray) json.get("list");
-        responseJson.put("id",((JSONObject)json.get("city")).get("id"));
-        responseJson.put("name",((JSONObject)json.get("city")).get("name"));
-        responseJson.put("country",((JSONObject)json.get("city")).get("country"));
-        int cantidad = 0;
-        List<Double> valoresTemperatura = new ArrayList<Double>();
-        int dia = 0;
-        String lastDate = "";
-        boolean primero = true;
-        JSONObject newDay = new JSONObject();
-        for(int index = 0; (dia < 5) && (index<listOfDays.size());index++){
-            JSONObject oneTemp = (JSONObject) listOfDays.get(index);
-
-            if (((String)oneTemp.get("dt_txt")).contains("03:00:00")){
-                if (primero){
-                    Date date = new Date();
-                    String modifiedDate = new SimpleDateFormat("yyyy-MM-dd").format(date);
-                    lastDate = modifiedDate;
-                    primero = false;
-                }
-                JSONObject oneMain = (JSONObject) oneTemp.get("main");
-                Double value = new Double(Double.parseDouble(oneMain.get("temp").toString())-273.15);
-                Double cantidadIteracion = 1.0;
-                for (Double s : valoresTemperatura) {
-                    value += s;
-                    cantidadIteracion++;
-                }
-                value = value / cantidadIteracion;
-                valoresTemperatura = new ArrayList<Double>();
-                newDay.put("nightTemp",value);
-                String date = ((String)oneTemp.get("dt_txt")).substring(0,10);
-                newDay.put("date",lastDate);
-                JSONArray oneWeather = (JSONArray) oneTemp.get("weather");
-                JSONObject weather = (JSONObject) oneWeather.get(0);
-                String icon = ((String)weather.get("icon"));
-                newDay.put("nightIcon",icon);
-                listOf5Days.add(newDay);
-                newDay = new JSONObject();
-                dia++;
-                cantidad++;
-            } else if (((String)oneTemp.get("dt_txt")).contains("15:00:00")){
-                JSONObject oneMain = (JSONObject) oneTemp.get("main");
-                Double value = new Double(Double.parseDouble(oneMain.get("temp").toString())-273.15);
-                Double cantidadIteracion = 1.0;
-                for (Double s : valoresTemperatura) {
-                    value += s;
-                    cantidadIteracion++;
-                }
-                value = value / cantidadIteracion;
-                valoresTemperatura = new ArrayList<Double>();
-                newDay.put("dayTemp",value);
-                String date = ((String)oneTemp.get("dt_txt")).substring(0,10);
-                newDay.put("date",date);
-                JSONArray oneWeather = (JSONArray) oneTemp.get("weather");
-                JSONObject weather = (JSONObject) oneWeather.get(0);
-                String icon = ((String)weather.get("icon"));
-                newDay.put("dayIcon",icon);
-                lastDate = date;
-                cantidad++;
-            } else {
-                JSONObject oneMain = (JSONObject) oneTemp.get("main");
-                Double value = new Double(Double.parseDouble(oneMain.get("temp").toString())-273.15);
-                valoresTemperatura.add(value);
-            }
-        }
-        if (!newDay.toString().equals("{}") ){
-            listOf5Days.add(newDay);
-        }
-        responseJson.put("info",listOf5Days);    
+        JSONObject responseJson = new JSONObject();
+        responseJson = setCityDetails(responseJson, weatherJson);
+        responseJson = setWeatherInfo(responseJson, weatherJson);
 
         return responseJson.toString();
     }
 
-    // public String forecastData(Long id) {
-    //     return openWeatherMapClient.getWeather(id);
-    // }
+    private JSONObject setWeatherInfo(JSONObject responseJson, JSONObject weatherJson) {
+        JSONArray listOfDays = (JSONArray) weatherJson.get("list");
+        JSONArray listOf5Days = new JSONArray();
+        Iterator listOfDaysIterator = listOfDays.iterator();
+        JSONObject currentDay;
+        currentDay = (JSONObject) listOfDaysIterator.next();
+        for (int days = 1; days <= 5; days++) {
+            JSONObject dayWeatherObject = new JSONObject();
+            String dateFromCurrentDay = currentDateFromDay(currentDay);
+            dayWeatherObject.put("date", dateFromCurrentDay);
+            WeatherValueData dayWeatherValueData = new WeatherValueData();
+            while (listOfDaysIterator.hasNext() && shouldAddDay(currentDay)) {
+                dayWeatherValueData = addDataToWeatherValueData(dayWeatherValueData, currentDay);
+                currentDay = (JSONObject) listOfDaysIterator.next();
+            }
+            WeatherValueData nightWeatherValueData = new WeatherValueData();
+            while (listOfDaysIterator.hasNext() && shouldAddNight(currentDay)) {
+                nightWeatherValueData = addDataToWeatherValueData(nightWeatherValueData, currentDay);
+                currentDay = (JSONObject) listOfDaysIterator.next();
+            }
+
+            dayWeatherObject = weatherValueData(dayWeatherObject, dayWeatherValueData, "dayTemp", "dayIcon");
+            dayWeatherObject = weatherValueData(dayWeatherObject, nightWeatherValueData, "nightTemp", "nightIcon");
+
+            listOf5Days.add(dayWeatherObject);
+        }
+
+        responseJson.put("info", listOf5Days);
+
+        return responseJson;
+    }
+
+    private WeatherValueData addDataToWeatherValueData(WeatherValueData weatherValueData, JSONObject currentDay) {
+        JSONObject mainData = (JSONObject) currentDay.get("main");
+        weatherValueData.temperatures.add(Double.parseDouble(mainData.get("temp").toString()) - 273.15);
+
+        JSONObject weatherData = (JSONObject) ((JSONArray) currentDay.get("weather")).get(0);
+        String dayIcon = weatherData.get("icon").toString();
+        weatherValueData.icons.add(dayIcon);
+
+        return weatherValueData;
+    }
+
+    private JSONObject weatherValueData(JSONObject dayWeatherObject, WeatherValueData weatherValueData, String typeOfTemp, String typeOfIcon) {
+        if (weatherValueData.temperatures.isEmpty()) {
+            return dayWeatherObject;
+        }
+        double totalTemperatures = weatherValueData.temperatures.size();
+        Iterator temperatureIterator = weatherValueData.temperatures.iterator();
+        double sumOfTemperatures = 0;
+        while (temperatureIterator.hasNext()) {
+            sumOfTemperatures += (Double) temperatureIterator.next();
+        }
+        double meanValue = sumOfTemperatures / totalTemperatures;
+        dayWeatherObject.put(typeOfTemp, meanValue);
+        dayWeatherObject.put(typeOfIcon, weatherValueData.icons.get(0));
+
+        return dayWeatherObject;
+    }
+
+    private boolean shouldAddNight(JSONObject currentDay) {
+        String input = currentDay.get("dt_txt").toString();
+        String[] elements = input.split(" ");
+        String hour = elements[1];
+        return (hour.equals("12:00:00")
+                || hour.equals("15:00:00")
+                || hour.equals("18:00:00")
+                || hour.equals("21:00:00")
+                || hour.equals("00:00:00"));
+    }
+
+    private boolean shouldAddDay(JSONObject currentDay) {
+        String input = currentDay.get("dt_txt").toString();
+        String[] elements = input.split(" ");
+        String hour = elements[1];
+        return (hour.equals("00:00:00")
+                || hour.equals("03:00:00")
+                || hour.equals("06:00:00")
+                || hour.equals("09:00:00")
+                || hour.equals("12:00:00"));
+    }
+
+    private String currentDateFromDay(JSONObject currentDay) {
+        String input = currentDay.get("dt_txt").toString();
+        String[] elements = input.split(" ");
+        return elements[0];
+    }
+
+    private JSONObject setCityDetails(JSONObject responseJson, JSONObject weatherJson) {
+        responseJson.put("id", ((JSONObject) weatherJson.get("city")).get("id"));
+        responseJson.put("name", ((JSONObject) weatherJson.get("city")).get("name"));
+        responseJson.put("country", ((JSONObject) weatherJson.get("city")).get("country"));
+
+        return responseJson;
+    }
+
+    public class WeatherValueData {
+        List<Double> temperatures = new ArrayList<>();
+        List<String> icons = new ArrayList<>();
+    }
+
 }
